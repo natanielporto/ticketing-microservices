b1b730add52b01f7a311dcd9fb2a589e7723e23c
Nataniel <nataniel@Nataniels-MBP.Home>
2024-07-18 00:23:40 +0100

    Updates

--- common/build/events/ticket-updated-event.d.ts
+++ common/build/events/ticket-updated-event.d.ts
@@ -1,4 +1,4 @@
-import { Subjects } from './subjects';
+import { Subjects } from "./subjects";
 export interface TicketUpdatedEvent {
     subject: Subjects.TicketUpdated;
     data: {
@@ -7,5 +7,6 @@ export interface TicketUpdatedEvent {
         title: string;
         price: number;
         userId: string;
+        orderId?: string;
     };
 }
